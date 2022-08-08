a2db87246928a73dc821d4a05a80497230f27e58
Jack Wallace <jackwillamwallace@hotmail.com>
2022-08-08 10:49:06 +0100

    completed!!!

--- src/objects.js
+++ src/objects.js
@@ -14,35 +14,48 @@ const getName = object => {
 };
 
 const getProperty = (property, object) => {
-  // your code here
-};
+  
+  return object[property]
+
+}; 
 
 const hasProperty = (property, object) => {
-  // your code here
+   return object.hasOwnProperty(property)
 };
 
 const isOver65 = person => {
-  // your code here
+  if (person.age <= 65) { 
+    return false
+  } else {
+    return true
+  }
 };
 
 const getAges = people => {
-  // your code here
+  return people.map((el) => {return el.age})
 };
 
 const findByName = (name, people) => {
-  // your code here
+  return people.find((person) => {return person.name === name})
 };
 
 const findHondas = cars => {
-  // your code here
+  return cars.filter((el) => el.manufacturer === 'Honda')
 };
 
 const averageAge = people => {
-  // your code here
+ 
+  return people.map((el) => {return el.age}).reduce((prev, accu) => prev + accu, 0)/people.length
+    
 };
 
 const createTalkingPerson = (name, age) => {
-  // your code here
+ return { 
+ name: name,
+ age: age,
+ introduce: function(friend) {return 'Hi ' + friend + ', my name is ' + this.name + ' and I am ' + this.age + '!'}
+ }; 
+
 };
 
 module.exports = {
